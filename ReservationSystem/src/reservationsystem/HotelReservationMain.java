eea4026953c04867323ae208395c78769703067c
juanrafaelCASANO <166791556+juanrafaelCASANO@users.noreply.github.com>
2024-06-26 00:02:37 +0800

    Update HotelReservationMain.java

--- ReservationSystem/src/reservationsystem/HotelReservationMain.java
+++ ReservationSystem/src/reservationsystem/HotelReservationMain.java
@@ -16,7 +16,7 @@ public class HotelReservationMain {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        new HotelReservation();
+        new reservation();
     }
     
 }
